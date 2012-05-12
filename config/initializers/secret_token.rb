6c6af5fbc43664c2cb7fb5af13cac3f49c55dc06
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-05-12 14:43:05 +0530

    initial commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppPostgressql::Application.config.secret_token = '3970847df0e267660703bb60e619f3dc971566774f556fa71529014ddac36ed29e034a95f94598fc088265beb198d7b25a406c9b1feeeab63867cca4cda221ad'
